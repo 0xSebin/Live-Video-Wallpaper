f5b1d321872b51903183331876ef6161f0d11c08
Sebin Thomas <56429311+0xSebin@users.noreply.github.com>
2021-08-29 14:16:44 +0530

    Update video.sh

--- video.sh
+++ video.sh
@@ -9,8 +9,8 @@ make clean
 cd ..
 apt install mpv
 clear
-echo -e "\e[1;31m  For video Wallpaper use this command \e[0m"
+echo -e "\e[1;31mFor video Wallpaper use this command \e[0m"
 echo -e "xwinwrap -fs -fdt -ni -b -nf -un -o 1.0 -debug -- mpv -wid WID --loop --no-audio ~/Videos/video.mp4 "
-echo -e "\e[1;31m  For gif wallpaper use this command  \e[0m"
+echo -e "\e[1;31mFor gif wallpaper use this command  \e[0m"
 echo -e "xwinwrap -fs -fdt -ni -b -nf -un -o 1.0 -debug -- gifview -w WID ~/mygif.gif -a "
-echo -e "  Change video and gif location while using "
+echo -e "Change video and gif location while using "
